65e9927550a9f12c2b215ceec9a6840edabf2e24
vledd <nechaev1701@gmail.com>
2024-11-04 16:51:08 +0200

    [TelegramWordcloud] Added initial helper functions

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+import enum
+
+# This should be upgraded in the future.
+# For example, I can't add '-' since it can be inside of a word.
+# TODO Please fix the algorithm in newer versions
+FORBIDDEN_CHAR: list[str] = [',', '.', '(', ')', '\n', '\'', '"', "*", '%', '&', '?', "!", "…"]
+
+
+class ParserSortWords(enum.Enum):
+    ASCENDING = 1,   # From least to most used
+    DESCENDING = 0,  # From most to least used
---
+++ src/telegram_parse_helpers.py
@@ -0,0 +1,52 @@
+import json
+import numpy
+from wordcloud import WordCloud, STOPWORDS
+from constants import FORBIDDEN_CHAR
+from constants import ParserSortWords
+
+
+def parse_json_chat(json_data: dict,
+                    min_word_size: int = 0,
+                    sorting: ParserSortWords = ParserSortWords.DESCENDING) -> list[tuple[str, int]]:
+    """
+    This function is used to parse JSON chat and return a list with tuples.
+    Every tuple contain a string with a word plus its frequency.
+    :param json_data: JSON chat loaded in Python. Please use json.loads() before this function.
+    :param min_word_size: You can pop too short words right at the moment of parsing if you want.
+    :param sorting: How to sort words -- from most used to least or vice-versa
+    :return: List of tuple pairs ("word": str, frequency: int).
+    """
+
+    words_stat: dict[str, int] = {}
+
+    for message in json_data["messages"]:  # Parse thru all messages
+        # Omit empty messages and nested messages (like links etc.)
+        if (type(message["text"]) is not list) and (message["text"] != ""):
+            # Split messages by whitespaces.
+            for word in message["text"].split(" "):
+                # Replace all forbidden characters.
+                # TODO see TODO in FORBIDDEN_CHAR variable. Fix it.
+                for char in FORBIDDEN_CHAR:
+                    word = word.replace(char, "")
+                # Create new dict. entry or add frequency to it.
+                if word not in words_stat:
+                    words_stat[word] = 1
+                else:
+                    words_stat[word] += 1
+
+    # Sort words in DESC or ASC order
+    should_reverse: bool = False
+    if sorting == ParserSortWords.DESCENDING:
+        should_reverse = True
+
+    words_list_clean: list[tuple[str, int]] = sorted(words_stat.items(), key=lambda x: x[1], reverse=should_reverse)
+
+    # Original dictionary is not needed anymore
+    del words_stat
+
+    # Pop too short words
+    for idx, word in enumerate(words_list_clean):
+        if len(word[0]) < min_word_size:
+            words_list_clean.pop(idx)
+
+    return words_list_clean
